0ad9861affae31d027a006c378a8d55d5fabe6eb
Loc dinh <locdx1102@gmail.com>
2024-06-08 20:25:19 +0700

    fix security

--- src/main/java/com/example/resourceservice/ResourceServiceApplication.java
+++ src/main/java/com/example/resourceservice/ResourceServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.resourceservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan(value = "com.example")
 public class ResourceServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/resourceservice/TestController.java
+++ src/main/java/com/example/resourceservice/TestController.java
@@ -1,5 +1,7 @@
 package com.example.resourceservice;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,13 @@ public class TestController {
     @PostMapping("/test")
     public void test(){
         System.out.println("test");
+    }
+
+  @PostMapping("/test1")
+    public void test1(){
+
+      System.out.println(SecurityContextHolder.getContext().getAuthentication().getAuthorities());
+        System.out.println("test1");
     }
 
 }
